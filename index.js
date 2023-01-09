e51c06931bcf87e994a1f2ec88a56ac74ec3548f
GurjeetSahu <gurjeetsahu91425@gmail.com>
2023-01-09 18:18:44 +0530

    scroll the slider

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value // Update the current slider value (each time you drag the slider handle) 
-slider.oninput = function () {
+output.innerHTML = slider.value;
+
+slider.addEventListener("wheel", function (e) {
+  if (e.deltaY < 0) {
+    slider.valueAsNumber += 0.1;
     output.innerHTML = slider.value;
     information = slider.value;
     chrome.tabs.executeScript(null, {
-        code: 'var config = "' + information + '";'
+      code: 'var config = "' + information + '";'
     }, function () {
-        chrome.tabs.executeScript(null, { code: 'document.getElementsByTagName("video")[0].playbackRate=config' });
+      chrome.tabs.executeScript(null, { code: 'document.getElementsByTagName("video")[0].playbackRate=config' });
     });
+  } else {
+    slider.value -= 0.1;
+    output.innerHTML = slider.value;
+    information = slider.value;
+    chrome.tabs.executeScript(null, {
+      code: 'var config = "' + information + '";'
+    }, function () {
+      chrome.tabs.executeScript(null, { code: 'document.getElementsByTagName("video")[0].playbackRate=config' });
+    });
+  }
+  e.preventDefault();
+  e.stopPropagation();
+})
+slider.oninput = function () {
+  output.innerHTML = slider.value;
+  information = slider.value;
+  chrome.tabs.executeScript(null, {
+    code: 'var config = "' + information + '";'
+  }, function () {
+    chrome.tabs.executeScript(null, { code: 'document.getElementsByTagName("video")[0].playbackRate=config' });
+  });
 }
